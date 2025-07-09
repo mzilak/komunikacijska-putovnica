a48cae6ee85a04c49ffbc5c8b8acdcb7e9a8a876
mzilak <57595384+mzilak@users.noreply.github.com>
2025-07-09 16:18:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+
+const previewImg = document.getElementById('preview');
+const removeBtn = document.getElementById('remove-photo');
+const pictogramSelect = document.getElementById('piktogram-select');
+document.getElementById('upload').addEventListener('change', function(e) {
+  const file = e.target.files[0];
+  if (!file) return;
+  const reader = new FileReader();
+  reader.onload = function(event) {
+    previewImg.src = event.target.result;
+    previewImg.classList.remove('hidden');
+    if (removeBtn) removeBtn.classList.remove('hidden');
+    if (pictogramSelect) pictogramSelect.value = "";
+  };
+  reader.readAsDataURL(file);
+  
+  
+});
+if (removeBtn) {
+  removeBtn.addEventListener('click', () => {
+    previewImg.src = '';
+    previewImg.classList.add('hidden');
+    removeBtn.classList.add('hidden');
+    document.getElementById('upload').value = '';
+  });
+}
+if (pictogramSelect) {
+  pictogramSelect.addEventListener('change', function() {
+    if (this.value) {
+      previewImg.src = 'assets/' + this.value;
+      previewImg.classList.remove('hidden');
+      if (removeBtn) removeBtn.classList.add('hidden');
+      document.getElementById('upload').value = '';
+    }
+  });
+}
+
+flatpickr("#datum", {
+  locale: "hr",
+  dateFormat: "d.m.Y.",  // DD.MM.GGGG
+  allowInput: true
+});
